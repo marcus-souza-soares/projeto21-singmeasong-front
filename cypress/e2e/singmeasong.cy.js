6370621dbeae3b3f17d9d9060210af81c5a1882a
Marcus <marcusadrenaline@gmail.com>
2022-09-23 18:46:40 -0300

    first commit

---
+++ cypress/e2e/singmeasong.cy.js
@@ -0,0 +1,11 @@
+/* eslint-disable no-undef */
+/// <reference types="cypress" />
+import { faker } from "@faker-js/faker"
+
+describe("Recomendações", () => {
+
+  it.todo("Deve criar uma nova recomendação", () => {
+    
+  })
+
+})
